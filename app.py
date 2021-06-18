85162edb48ece6b6bb6cb947d696969bf7c0d67c
Joshua <BlackhamJoshua@comcast.net>
2021-06-17 21:52:08 -0600

    working commit

--- app.py
+++ app.py
@@ -1,7 +1,31 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+import json
+import pandas as pd
+from fbprophet import Prophet
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy   dog'
+app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route("/")
+cors = CORS(app, resources={r"/forcast": {"origins": "*"}})
+
+@app.route("/forcast", methods=["POST"])
+@cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
+
 def index():
-    return "Hello World!"
+    data = json.loads(request.data.decode())['data']
+    df = pd.DataFrame(data[1:], columns = data[0])
+    df.head()
+    df.describe()
+    df = df[["Date","Open"]]
+    df = df.rename(columns={"Date":"ds","Open":"y" })
+    df.head()
+    m = Prophet(yearly_seasonality=True, daily_seasonality=False)
+    m.fit(df)
+    future = m.make_future_dataframe(periods = 90, include_history = False)
+    prediction = m.predict(future)
+    prediction = prediction.rename(columns={"ds":"Date"})
+    prediction = prediction[['Date', 'trend', 'trend_lower', 'trend_upper']]
+    csvData = prediction.to_csv()
+    return jsonify({'data': csvData})
