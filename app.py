5ae586e0bce16b8662508a5b1c3575164bd992ac
Joshua <BlackhamJoshua@comcast.net>
2021-06-21 16:20:18 -0600

    CORS issues

--- app.py
+++ app.py
@@ -21,9 +21,9 @@ def index():
     df = df[["Date","Open"]]
     df = df.rename(columns={"Date":"ds","Open":"y" })
     df.head()
-    m = Prophet(yearly_seasonality = "auto", daily_seasonality = "auto")
+    m = Prophet(yearly_seasonality = "auto", weekly_seasonality= "auto", daily_seasonality = "auto")
     m.fit(df)
-    future = m.make_future_dataframe(periods = 90, include_history = True)
+    future = m.make_future_dataframe(periods = 90, include_history = False)
     prediction = m.predict(future)
     prediction = prediction.rename(columns = {"ds":"Date"})
     prediction = prediction[['Date', 'trend', 'trend_lower', 'trend_upper']]
